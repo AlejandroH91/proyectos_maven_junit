cb83ead70773d814e0b00dc32dd0f93f7a9a9490
Alejandro <hernandez.razo.alejandro@iescamas.es>
2025-01-21 12:47:03 +0100

    Pruebas test termometro a mitad

--- termometro_proyecto/src/test/java/com/endes/instrumento/TermometroTest.java
+++ termometro_proyecto/src/test/java/com/endes/instrumento/TermometroTest.java
@@ -3,24 +3,55 @@ package com.endes.instrumento;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TermometroTest {
 
-	private  int temp= 40;
+	private Termometro termometro;
 	
 	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("Antes de todas las pruebas");
+	}
 	
+	@BeforeEach
+	void setUp() throws Exception{
+		termometro = new Termometro(0);
+	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName ("Prueba del constructor")
+	void testConstructor() {
+		double resultadoEsperado = 0;
+		double resultado= termometro.getTemperaturaCelsius();
+		assertEquals (resultadoEsperado, resultado, "No es igual al obtener la temperatura");
+	
 	}
 	@Test
 	@DisplayName ("Devuelve la temperatura")
 	public void ajustarTemperatura() {
+		double resultadoEsperado= 1;
+		 termometro.ajustarTemperatura(1);
+		 assertEquals (resultadoEsperado, termometro.getTemperaturaCelsius(), "No es igual al obtener la temperatura");
+	}
+	
+	@Test
+	@DisplayName ("Devuelve la temperatura en celsius")
+	public void setTemperaturaCelsius() {
+		double resultadoEsperado= 10;
+		termometro.setTemperaturaCelsius(10);
+		assertEquals (resultadoEsperado, termometro.getTemperaturaCelsius(), "No es igual al obtener la temperatura");
 		
 	}
+	
+	@Test
+	@DisplayName("Está en ebullición")
+	public void esEbullicion() {
+		
+	}
+	
+
 
 }
